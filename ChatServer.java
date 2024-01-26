5a233127f7c8bf80dfdcd5eb98f38149993a05c2
ayah825 <156377548+ayah825@users.noreply.github.com>
2024-01-25 17:15:51 -0800

    Add files via upload

---
+++ ChatServer.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList; 
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    ArrayList<String> messages = new ArrayList<>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            for (int i = 0; i < messages.size(); i++) {
+                return String.format(messages.get(i) + "\n");
+            }
+            return null; 
+        } else {
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    messages.add(parameters[1]);
+                    String temp = parameters[1];
+                    return temp + "has been added";
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class ChatServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
